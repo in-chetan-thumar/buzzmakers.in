589f65bcda2fd7189fd2769f634b6a3cbba5643d
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-03-09 08:44:23 +0530

    Committed Lumar HTML

---
+++ www/public/LLumar-html/js/custom.js
@@ -0,0 +1,84 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 500) {
+     $('header').addClass('sticky');
+     $('header').addClass('transition');
+ }
+ else {
+     $('header').removeClass('sticky');
+     $('header').removeClass('transition');
+ }
+});
+
+
+$(document).ready(function ($) {
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+});
+
+
+
+
+
+//LOADER
+
+  $(window).on('load', function(){
+
+    $("#preloader").removeClass("loader_show");
+
+    $("#preloader").addClass("hide");
+
+    $(".loader").addClass("fadeout");
+
+  });
+
+
+
+
+
+if($(window))
+{
+    new WOW().init();
+}
+
+
+
+
+
+$('input[data-list]').each(function () {
+  var availableTags = $('#' + $(this).attr("data-list")).find('option').map(function () {
+    return this.value;
+  }).get();
+
+  $(this).autocomplete({
+    source: availableTags
+  }).on('focus', function () {
+    $(this).autocomplete('search', ' ');
+  }).on('search', function () {
+    if ($(this).val() === '') {
+      $(this).autocomplete('search', ' ');
+    }
+  });
+});
