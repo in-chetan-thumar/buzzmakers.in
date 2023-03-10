6806fcf9f0247373574dc2b23adece745dc64663
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-03-10 09:47:02 +0530

    Added new changes

--- www/public/LLumar-html/js/custom.js
+++ www/public/LLumar-html/js/custom.js
@@ -65,20 +65,3 @@ if($(window))
 
 
 
-
-
-$('input[data-list]').each(function () {
-  var availableTags = $('#' + $(this).attr("data-list")).find('option').map(function () {
-    return this.value;
-  }).get();
-
-  $(this).autocomplete({
-    source: availableTags
-  }).on('focus', function () {
-    $(this).autocomplete('search', ' ');
-  }).on('search', function () {
-    if ($(this).val() === '') {
-      $(this).autocomplete('search', ' ');
-    }
-  });
-});
