ec302c41f5f37b4f85d83fc9f4d3820b562aced6
Kajal baldha <kajal.baldha@tiez.nl>
2023-07-21 10:36:59 +0530

    main changes

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -17,10 +17,10 @@
               <a class="nav-link" href={{route('frontend.about')}}>About Us</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false" >
-                Services
-              </a>
+{{--              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"--}}
+{{--                aria-expanded="false" >--}}
+{{--                Services--}}
+{{--              </a>--}}
               <ul class="dropdown-menu">
                 <li class="arrow-up">
                   <div></div>
@@ -70,9 +70,9 @@
             <a class="nav-link" href={{route('frontend.about')}}>About Us</a>
         </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Services
-        </a>
+{{--        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">--}}
+{{--          Services--}}
+{{--        </a>--}}
         <ul class="dropdown-menu">
           <li><a class="dropdown-item" href={{route('frontend.services.strategy')}}>Branding</a></li>
           <li><a class="dropdown-item" href={{route('frontend.services.content')}}>Marketing</a></li>
